c57785a5734c2d22365967596b152fa2b736271f
Trend20 <enockomondi305@gmail.com>
2021-05-10 11:58:54 +0300

    adding responsive view

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 //Get the button:
-let myButton = document.getElementById("myBtn");
+let myButton = document.getElementById('myBtn');
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function () {
+	scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
-    }
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		myButton.style.display = 'block';
+	} else {
+		myButton.style.display = 'none';
+	}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
-
