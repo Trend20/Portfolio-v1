abbf8729088b187c63b633b8975907816b2d662c
DevEnock <enockomondi305@gmail.com>
2021-09-27 16:18:37 +0300

    updating the page contents

--- js/main.js
+++ js/main.js
@@ -19,3 +19,22 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+
+
+// navbar background color
+let myNavbar = document.getElementById('header');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
+
+
